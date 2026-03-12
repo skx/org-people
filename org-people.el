d0db5eb3cfe4bfae2d0d1049fabe6963c1342a14
Steve Kemp <steve@steve.org.uk>
2026-03-13 00:45:25 +0200

    chore: Automate version bumping.
    Using https://git-scm.com/docs/gitattributes

--- org-people.el
+++ org-people.el
@@ -4,7 +4,7 @@
 
 ;; Author: Steve Kemp <steve@steve.fi>
 ;; Maintainer: Steve Kemp <steve@steve.fi>
-;; Version: 2.2
+;; Version: $Id$
 ;; Package-Requires: ((emacs "29.1") (org "9.0"))
 ;; Keywords: outlines, contacts, people
 ;; URL: https://github.com/skx/org-people
